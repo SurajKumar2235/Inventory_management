aabfb2f12d5e53af3536d1585556c367c1f2c2be
SurajKumar2235 <botvirus2235@gmail.com>
2024-04-10 22:14:39 +0530

    added employee

--- INVENTORY/models.py
+++ INVENTORY/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+# ======================================================================
 
 class Inventory(models.Model):
     name=models.CharField(max_length=200,null=False,blank=False)
@@ -14,7 +15,7 @@ class Inventory(models.Model):
     
     class Meta:
         db_table='Inventory'
-
+# ======================================================================
 class Category(models.Model):
     name=models.CharField(max_length=200)
     def __str__(self):
@@ -23,4 +24,35 @@ class Category(models.Model):
 
     class Meta:
         db_table='Category'
+# ======================================================================
+
+
+class Employees(models.Model):
+    name=models.CharField(max_length=200,null=False,blank=False)
+    quantity=models.IntegerField()
+    category=models.ForeignKey('Category',on_delete=models.SET_NULL,blank=True,null=True)
+    date_created=models.DateTimeField(auto_now_add=True)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name   
+
+    class Meta:
+        db_table='employee'
+
+# ======================================================================
+
+class inventory_History(models.Model):
+    item_name=models.CharField(max_length=200,null=False,blank=False)
+    item=models.ForeignKey(Inventory,on_delete=models.SET_NULL, blank=True, null=True)
+    qty_purchased=models.IntegerField()
+    price=models.ForeignKey('Category',on_delete=models.SET_NULL,blank=True,null=True)
+    date_of_purchased=models.DateTimeField(auto_now_add=True)
+    employee=models.ForeignKey(Employees,on_delete=models.CASCADE)
+    is_deleted=models.BooleanField(default=False)
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        db_table='Inventory_history'
 
